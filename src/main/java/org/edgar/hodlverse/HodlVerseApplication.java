c5d0c9ce9b0530f422f0cf2c173dadf12ef1a553
edgar565 <edgarsancheznicolau@gmail.com>
2025-01-23 11:50:28 +0100

    Primer Commit

---
+++ src/main/java/org/edgar/hodlverse/HodlVerseApplication.java
@@ -0,0 +1,13 @@
+package org.edgar.hodlverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HodlVerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HodlVerseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/edgar/hodlverse/HodlVerseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.edgar.hodlverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HodlVerseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
